fa08affa5939c4d2567a842aae78a10dea9fdd78
Marissa Reinke <reinke.marissa@gmail.com>
2017-04-19 19:50:29 -0600

    initial setup

--- turing/flash-cards/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import { expect } from 'chai';
-import { assert } from 'chai';
-import Guess from '../scripts/guess'
-import Card from '../scripts/card'
-import Deck from '../scripts/deck'
-import Round from '../scripts/round'
