caa8284ef20e5244df51fe8bec841fa4139df1d4
Marissa Reinke <reinke.marissa@gmail.com>
2017-04-19 21:06:10 -0600

    setup first round of logic

--- anaglyphic-text-effect/index.js
+++ anaglyphic-text-effect/index.js
@@ -1,7 +1,17 @@
 const submitBtn = document.getElementById('submit-btn');
+const h1 = document.querySelector('h1');
+
+if (!h1.dataset.content) {
+  h1.dataset.content = 'Marissa'
+}
 
 submitBtn.addEventListener('click', function() {
   const inputField = document.getElementById('input-field');
+
+  h1.dataset.content = inputField.value;
+
   const title = document.getElementById('title');
+
   title.innerText = inputField.value;
+
 });
